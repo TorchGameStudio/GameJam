5a70a3a4d1bd997d4ee7c03a059288d13dae2793
Tyler Marriott <marriotttyler7@gmail.com>
2014-12-13 11:21:19 -0500

    -Increased the size of everything in the screen by a factor of 10 -Changed Canvas to be in the camera screen space, so it would stop blocking our view of the player when jumping -Upped the forces exerted on the player to match the new scale - Added ray casting from the player to detect if standing on something -Can now jump on any object -horizontal movement in the air is limited

--- UnityTestProject/Assets/PlayerController.cs
+++ UnityTestProject/Assets/PlayerController.cs
@@ -7,42 +7,26 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	#region fields
 	Rigidbody2D rigidBody;
-	readonly Vector2 PUSHFORCE = new Vector2(25.0f,0.0f);
-	readonly Vector2 JUMPFORCE = new Vector2(0.0f, 50.0f);
-	readonly float MAXSPEED = 20.0f;
-	bool _isGrounded;
+	readonly Vector2 PUSHFORCE = new Vector2(200.0f,0.0f);
+	readonly Vector2 JUMPFORCE = new Vector2(0.0f, 500.0f);
+	const float MAXSPEED = 60.0f;
 	#endregion
 	#region Monobehaviour Functions
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 
 		if(rigidbody2D == null)
 		{
 			gameObject.AddComponent<Rigidbody2D>();
 		}
 		rigidBody = this.GetComponent<Rigidbody2D>() as Rigidbody2D;
-        
 	}
-		void OnCollisionEnter2D(Collision2D collider)
-	{
-		if(collider.gameObject.tag.Equals("Ground"))
-		{
-			_isGrounded = true;
-		}  
-
-	}
-	void OnCollisionExit2D(Collision2D collider)
-	{
-		if(collider.gameObject.tag.Equals("Ground"))
-		{
-			_isGrounded = false;
-		}
-	}
+		
 
 	
 	// Update is called once per frame
 	void Update () {
-		if(_isGrounded)
+		if(isGrounded())
 		{
 			if(Mathf.Abs(rigidBody.velocity.x) < MAXSPEED)//Can't turn when going to fast or speed up
 			{
@@ -64,10 +48,42 @@ public class PlayerController : MonoBehaviour {
 				rigidBody.AddForce(-JUMPFORCE/4.0f);
 			}
 		}
+		else
+		{
+			if(Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				rigidBody.AddForce(PUSHFORCE/4.0f);
+			}
+			else if(Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				rigidBody.AddForce(-PUSHFORCE/4.0f);
+			}
+		}
 
 
 	
 	}
 
+	#endregion
+	#region Private Functions
+	bool isGrounded()
+	{
+		//use renderer to find the base of the gameobject 
+		Vector2 origin = new Vector3(transform.position.x,transform.position.y - (renderer.bounds.max.y/1.5f));
+		//cast ray down for a short distance
+		RaycastHit2D hit = Physics2D.Raycast(origin,-Vector2.up,0.1f);
+		//Debug.DrawRay(origin,-Vector2.up,Color.red,1);
+		if(hit.collider != null)
+		{
+			//Debug.Log("GROUNDED " + hit.collider.gameObject.name);
+			return true;
+
+		}
+		else
+		{
+			//Debug.Log("AIR TIME");
+			return false;
+		}
+	}
 	#endregion
 }
--- UnityTestProject/Assets/goalPost.cs
+++ UnityTestProject/Assets/goalPost.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class goalPost : MonoBehaviour {
 	#region fields
 	public Text scoreSpot; //The Text GameObject
-	public PolygonCollider2D groundCollider;
+	PolygonCollider2D groundCollider;
 	uint score = 0;
 	const uint goalPoints = 1;
 	BoxCollider2D myCollider;
@@ -21,13 +21,13 @@ public class goalPost : MonoBehaviour {
 		myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
 		//set up the collider so that it is in the back of the net, and a third of the nets width
 		//myCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
-		myCollider.center.Set(gameObject.transform.position.x/3.0f,gameObject.transform.position.y);
-		myCollider.size.Set(0.6f,renderer.bounds.size.y); //bounds.SetMinMax(new Vector3(renderer.bounds.min.x,renderer.bounds.min.y,renderer.bounds.min.z),new Vector3(renderer.bounds.max.x/3,renderer.bounds.max.y,renderer.bounds.max.z));
-
-
-		collider2D.bounds.center.Set(renderer.bounds.center.x,renderer.bounds.center.y,0);
-		collider2D.bounds.size.Set(renderer.bounds.size.x/3,renderer.bounds.size.y,0);
-		collider2D.bounds.SetMinMax(renderer.bounds.min,renderer.bounds.max);
+//		myCollider.center.Set(gameObject.transform.position.x/3.0f,gameObject.transform.position.y);
+//		myCollider.size.Set(0.6f,renderer.bounds.size.y); //bounds.SetMinMax(new Vector3(renderer.bounds.min.x,renderer.bounds.min.y,renderer.bounds.min.z),new Vector3(renderer.bounds.max.x/3,renderer.bounds.max.y,renderer.bounds.max.z));
+//
+//
+//		collider2D.bounds.center.Set(renderer.bounds.center.x,renderer.bounds.center.y,0);
+//		collider2D.bounds.size.Set(renderer.bounds.size.x/3,renderer.bounds.size.y,0);
+//		collider2D.bounds.SetMinMax(renderer.bounds.min,renderer.bounds.max);
 
 		groundCollider = GameObject.FindGameObjectWithTag("Ground").GetComponent<PolygonCollider2D>() as PolygonCollider2D; 
 
