b886cda90e220b6c02989acb46b0c5755bde7fc1
Tyler Marriott <marriotttyler7@gmail.com>
2014-12-13 13:15:10 -0500

    -Camera Follows Player(Precisely) Change to smash bros or mario bros camera later if time permitted -Level Bounds added. Anything that leaves the bounds is either respond if it is Spawnable or destroyed.

---
+++ UnityTestProject/Assets/Scripts/LevelBounds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBounds : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter2D(Collider2D collider)
+	{
+		ISpawnable spawnalbe = (ISpawnable)collider.gameObject.GetComponent(typeof(ISpawnable));
+		if(spawnalbe != null)
+		{
+			spawnalbe.ReSpawn();
+		}
+
+	}
+}
